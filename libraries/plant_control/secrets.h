fae98686590592c88d83c04a491e143c5d5f07fd
Jose <37120821+zerta92@users.noreply.github.com>
2023-10-21 17:52:23 +0100

    Delete secrets.h

--- libraries/plant_control/secrets.h
+++
@@ -1,9 +0,0 @@
-const char DEVICE_LOGIN_NAME[]  = "7cf1b4bf-a501-498f-ad47-ab2e54f2adc3";
-
-// Wifi and Server
-const char *SECRET_SSID = "The Cave";
-const char *SECRET_SPASSWORD = "iwantwifiplease";
-
-// Telegram
-#define SECRET_BOT_TOKEN "5085933205:AAGPxkWO4kgpxgl2B-8zWV2gKyk_7hQebJo"
-#define SECRET_CHAT_ID "1731352194"
