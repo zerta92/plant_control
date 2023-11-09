f4680c532edb6966a72057aba6f6b035e45754a8
Jose <37120821+zerta92@users.noreply.github.com>
2023-11-09 18:26:49 +0000

    in file OTA updates

--- plant_control.ino
+++ plant_control.ino
@@ -8,15 +8,15 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 #include <ESPmDNS.h>
+#include <ArduinoOTA.h>
 #include <WiFiUdp.h>
-// #include <ArduinoOTA.h>
 #include <HTTPClient.h>
 #include <UniversalTelegramBot.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 // Custom Files
-#include "handleOTA.h"
+// #include "handleOTA.h"
 #include "secrets.h"
 #include "GlobalVars.h"
 #include "ServerSetup.h"
@@ -68,6 +68,31 @@ void setup()
   Serial.println("Connecting Wifi...");
   connectWifi();
   Serial.println("starting server...");
+  ArduinoOTA
+      .onStart([]()
+               {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+                type = "sketch";
+            else // U_SPIFFS
+                type = "filesystem";
+
+            // NOTE: if updating SPIFFS, this would be the place to unmount SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+
+  ArduinoOTA.begin();
   setupServer(humiditySensor, humidityRelay, tempRelay, points_read_from_start, temperature, humidity_percent, humidity_setpoint_global, auto_mode, temp_nofo_flag, humidity_nofo_flag, is_pulse_water, no_water_detected);
   populateDataArrays();
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -203,6 +228,7 @@ void connectWifi()
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // /*use mdns for host name resolution*/
   if (MDNS.begin("esp32"))
   {
     Serial.println("MDNS responder started");
